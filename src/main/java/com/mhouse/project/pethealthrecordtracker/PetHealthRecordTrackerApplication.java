be9b310feb625c9af6b5ce49179d2b54e362ef61
Matthew House <matthewhouse@Matthews-MBP-2.attlocal.net>
2019-12-26 20:31:17 -0500

    Initial commit

---
+++ src/main/java/com/mhouse/project/pethealthrecordtracker/PetHealthRecordTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mhouse.project.pethealthrecordtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHealthRecordTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHealthRecordTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhouse/project/pethealthrecordtracker/PetHealthRecordTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhouse.project.pethealthrecordtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHealthRecordTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
